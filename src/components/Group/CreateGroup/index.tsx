4c765bfcb6211cdfc668ba1e8676917d6df384b8
WillFortes <willfortes.dev@gmail.com>
2024-03-25 10:12:23 -0300

    Items refactor

--- src/components/Group/CreateGroup/index.tsx
+++ src/components/Group/CreateGroup/index.tsx
@@ -73,11 +73,27 @@ const CreateGroup = ({show, setShow}: ICreateProps) => {
                     </div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                    <label htmlFor="items" className="block text-sm font-medium leading-6 text-gray-900">
                         Items
                     </label>
                     <div className="mt-2">
-                        <input {...register("items")} type="text" id="Items" className="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Items"/>
+                    <div className="mt-2 flex rounded-md shadow-sm">
+                        <div className="relative flex flex-grow items-stretch focus-within:z-10">
+                            <input
+                                type="text"
+                                name="items"
+                                id="items"
+                                className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Tags"
+                            />
+                        </div>
+                            <button
+                                type="button"
+                                className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                >
+                                <PlusIcon className="-ml-0.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                            </button>
+                    </div>
                     </div>
                 </div>
                 <div className="mb-3">
--- src/components/Group/UpdateGroup/index.tsx
+++ src/components/Group/UpdateGroup/index.tsx
@@ -82,7 +82,12 @@ const UpdateGroup = ({show, setShow, group}: IUpdateProps) => {
                         Items
                     </label>
                     <div className="mt-2">
-                        <input {...register("items")} type="text" id="Items" className="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Items"/>
+                        {/* <input {...register("items")} type="text" id="Items" className="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Items"/> */}
+                        <ul>
+                            {group.items && group.items.map((item) => (
+                                <li key={item.id}>{item.payer.payer_name}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className="mb-3">
