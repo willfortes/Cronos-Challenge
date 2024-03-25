f85a6631edb66511771006dea2e9ba68a857f157
WillFortes <willfortes.dev@gmail.com>
2024-03-25 10:13:20 -0300

    Items form create placeholder

--- src/components/Group/CreateGroup/index.tsx
+++ src/components/Group/CreateGroup/index.tsx
@@ -84,7 +84,7 @@ const CreateGroup = ({show, setShow}: ICreateProps) => {
                                 name="items"
                                 id="items"
                                 className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                placeholder="Tags"
+                                placeholder="Items"
                             />
                         </div>
                             <button
